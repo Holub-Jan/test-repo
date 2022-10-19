a6557b6e217047bb04719dcac232f57c9d71fb48
Holub-Jan <113909251+Holub-Jan@users.noreply.github.com>
2022-10-19 14:38:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
+import subprocess
 
 default_credential = DefaultAzureCredential()
 
-client = BlobServiceClient('jan.holub@1pf.onmicrosoft.com', credential=default_credential)
+#client = BlobServiceClient('jan.holub@1pf.onmicrosoft.com', credential=default_credential)
 
-print(client)
+try:
+    subprocess.check_output('az','account','show')
+except subprocess.CalledProcessError as e:
+    print(e.output)
