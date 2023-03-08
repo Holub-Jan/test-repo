1057165082070e4d4bcd9749bdf245c77ae82c33
Jan <jan.holub@1pf.cz>
2023-03-08 10:52:54 +0100

    cehcking path

--- convert/convertor.py
+++ convert/convertor.py
@@ -6,6 +6,6 @@ class CustomConvert:
         self._file_list = ['CHANGELOG.md', 'RELEASE-NOTES.md']
 
     def main(self):
-        print('curr path stuff',os.listdir(os.path.realpath('..')))
+        print('curr path stuff', os.listdir(os.getcwd()))
 
         print('curr path: ', os.path.abspath(os.getcwd()))
