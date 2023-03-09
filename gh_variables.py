52df58b102a4cc0883e0bce7d8a82119e8947c45
Jan <jan.holub@1pf.cz>
2023-03-09 15:30:40 +0100

    pr trigger

--- gh_variables.py
+++ gh_variables.py
@@ -5,10 +5,12 @@ def main():
     email = os.environ.get('USER_EMAIL')
     description = os.environ.get('DESCRIPTION')
     username = os.environ.get('USERNAME')
+    test_var = os.environ.get('TEST_VAR')
 
     print('Email: ', email)
     print('Description: ', description)
     print('Username: ', username)
+    print('Test_var: ', test_var)
 
     cs = CustomConvert()
 
