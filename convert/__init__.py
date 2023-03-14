fe97136be67681849f888fef35bc81af3ed0e5ae
Jan <jan.holub@1pf.cz>
2023-03-14 09:38:03 +0100

    feat:changelog reading

---
+++ convert/__init__.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    from convert.convertor import CustomConvert
+
+    cs = CustomConvert()
+
+    cs.main()
--- convert/convertor.py
+++ convert/convertor.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 class CustomConvert:
@@ -6,18 +7,22 @@ class CustomConvert:
         self._file_list = ['CHANGELOG.md', 'RELEASE-NOTES.md']
 
     def main(self):
-
         file_name = self._file_present()
         if not file_name:
             print('File "CHANGELOG.md" or "RELEASE-NOTES.md" not found!')
-            return
+            sys.exit(0)
 
         with open(os.path.abspath(os.getcwd())+'/'+file_name, 'r') as file:
             for line in file.readlines():
-                print(line)
+                if line != '':
+                    print(line)
 
     def _file_present(self):
         main_dir = os.listdir(os.getcwd())
         for file in self._file_list:
             if file in main_dir:
                 return file
+
+
+if __name__ == "__main__":
+    pass
