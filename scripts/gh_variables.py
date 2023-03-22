ba4ea76eebafcb5b5711321cf279de32f0e86b37
Jan <jan.holub@1pf.cz>
2023-03-22 12:36:19 +0100

    test: empty env variables

--- scripts/gh_variables.py
+++ scripts/gh_variables.py
@@ -1,37 +1,11 @@
 def main():
-    from change_log_reader.change_log_reader import CHReader
 
     import os
 
-    email = os.environ.get('USER_EMAIL')
-    description = os.environ.get('DESCRIPTION')
-    username = os.environ.get('USERNAME')
-    test_var = os.environ.get('TEST_VAR')
-    repo = os.environ.get('REPO')
-    print(repo)
-    print('Email: ', email)
-    print('Description: ', description)
-    print('Username: ', username)
-    print('Test_var: ', test_var)
-    chreader = CHReader(['CHANGELOG.md', 'RELEASE-NOTES.md'])
+    input_token = os.environ.get('INPUT_TOKEN')
+    secret_token = os.environ.get('SECRET_TOKEN')
 
-    ch_data = chreader.get_transcript()
-    file_name = chreader.get_file_name()
-    print(file_name)
-
-    if not ch_data:
-        description = 'Nasazení nové konfigurace aplikace'
-    else:
-        latest_version = ch_data[0]
-        version = latest_version.version
-        if latest_version.features and latest_version.bug_fixes:
-            description = f'Přidání nových fukncí a opravení chyb s verzí {version}'
-        elif not latest_version.features and latest_version.bug_fixes:
-            description = f'Opravení chyb s verzí {version}'
-        elif latest_version.features and not latest_version.bug_fixes:
-            description = f'Přidání nových fukncí s verzí {version}'
-
-    print(description)
+    print([input_token, secret_token])
 
 
 if __name__ == '__main__':
